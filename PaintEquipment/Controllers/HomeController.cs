9d9de731f6cfe57ca3076dfd6d8f3cf7a5a78ba1
igor egorov <igor127@bk.ru>
2022-01-24 15:20:15 +0300

    deleted bug in taghelper, insert first bottstrap visual in views

--- PaintEquipment/Controllers/HomeController.cs
+++ PaintEquipment/Controllers/HomeController.cs
@@ -7,12 +7,13 @@ namespace PaintEquipment.Controllers
 {
     public class HomeController : Controller
     {
-        IAppRepository repository;
+        private IAppRepository repository;
+        public int PageSize = 4;
         public HomeController(IAppRepository repo)
         { 
         repository = repo;
         }
-        public int PageSize = 4;
+       
         public ViewResult Index(int numerPage = 1) => View(new ProductListViewModel {
             Products = repository.Products.OrderBy(p => p.Id).Skip((numerPage - 1) * PageSize).Take(PageSize),
             PageInfo = new PageInfo {

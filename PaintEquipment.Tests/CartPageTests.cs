78ae9145c828b0922569a939d02cc58fa6c21349
igor egorov <igor127@bk.ru>
2022-01-27 12:51:08 +0300

    before ended cart

---
+++ PaintEquipment.Tests/CartPageTests.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Routing;
+using Moq;
+using PaintEquipment.Models;
+using PaintEquipment.Pages;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using Xunit;
+
+namespace PaintEquipment.Tests
+{
+    public class CartPageTests
+    {
+        [Fact]
+        public void Can_Load_Cart()
+        {
+
+            // Arrange
+            // - create a mock repository
+            Product p1 = new Product { Id = 1, Name = "P1" };
+            Product p2 = new Product { Id = 2, Name = "P2" };
+            Mock<IAppRepository> mockRepo = new Mock<IAppRepository>();
+            mockRepo.Setup(m => m.Products).Returns((new Product[] {
+        p1, p2
+    }).AsQueryable<Product>());
+
+            // - create a cart 
+            Cart testCart = new Cart();
+            testCart.AddRow(p1, 2);
+            testCart.AddRow(p2, 1);
+
+            // Action
+            CartModel cartModel = new CartModel(mockRepo.Object, testCart);
+            cartModel.OnGet("myUrl");
+
+            //Assert
+            Assert.Equal(2, cartModel.Cart.Rows.Count());
+            Assert.Equal("myUrl", cartModel.ReturnUrl);
+        }
+
+        [Fact]
+        public void Can_Update_Cart()
+        {
+            // Arrange
+            // - create a mock repository
+            Mock<IAppRepository> mockRepo = new Mock<IAppRepository>();
+            mockRepo.Setup(m => m.Products).Returns((new Product[] {
+        new Product { Id = 1, Name = "P1" }
+    }).AsQueryable<Product>());
+
+            Cart testCart = new Cart();
+
+
+            // Action
+            CartModel cartModel = new CartModel(mockRepo.Object, testCart);
+            cartModel.OnPost(1, "myUrl");
+
+            //Assert
+            Assert.Single(testCart.Rows);
+            Assert.Equal("P1", testCart.Rows.First().Product.Name);
+            Assert.Equal(1, testCart.Rows.First().Quantity);
+        }
+
+
+    }
+}

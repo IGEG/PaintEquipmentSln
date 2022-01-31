327ce5afcec30d3969cd4dd47453082cb49a1f2f
igor egorov <igor127@bk.ru>
2022-01-31 15:37:57 +0300

    create admincontroll

---
+++ PaintEquipment.Tests/AdminControllerTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using PaintEquipment.Models;
+using PaintEquipment.Controllers;
+using Moq;
+using Xunit;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PaintEquipment.Tests
+{
+    public class AdminControllerTests
+    {
+        [Fact]
+        public void CanGetProductInView()
+        {
+            Mock<IAppRepository> mock = new Mock<IAppRepository>();
+            mock.Setup(p => p.Products).Returns((new Product[]{
+            new Product { Id=1,Name="P1"},
+            new Product { Id = 2, Name = "P2" },
+            new Product { Id = 3, Name = "P3" },
+            }).AsQueryable<Product>());
+            AdminController controller = new AdminController(mock.Object);
+            Product[] products = GetViewModel<IEnumerable<Product>>(controller.Index())?.ToArray();
+            Assert.Equal(3, products.Length);
+            Assert.Equal("P2", products[1].Name);
+
+
+        }
+        private T GetViewModel<T>(IActionResult result) where T : class
+        {
+            return (result as ViewResult)?.ViewData.Model as T;
+        }
+    }
+}
---
+++ PaintEquipment/Controllers/AdminController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PaintEquipment.Models;
+
+namespace PaintEquipment.Controllers
+{
+    public class AdminController:Controller
+    {
+        IAppRepository repository;
+        public AdminController(IAppRepository repo)
+        {
+            repository = repo;
+        }
+        public ViewResult Index() => View(repository.Products);
+    }
+}

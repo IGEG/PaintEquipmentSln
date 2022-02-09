80d2b6f1a78d758948be237308d4f15423a4d623
igor egorov <igor127@bk.ru>
2022-02-09 22:29:18 +0300

    bewfore create maincontroller

---
+++ PaintEquipment/Controllers/MainController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PaintEquipment.Models;
+
+namespace PaintEquipment.Controllers
+{
+    public class MainController:Controller
+    {
+        AppDbContext AppDbContext;
+
+        public MainController(AppDbContext appDb)
+        {
+            AppDbContext = appDb;
+
+        }
+        public ViewResult Index() =>  View("Hello World");
+    }
+}
---
+++ PaintEquipment/Migrations/20220207182814_mymigr.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PaintEquipment.Migrations
+{
+    public partial class mymigr : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}

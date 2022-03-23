bb7876b63713b3a71b04ebe2042552c233c0c0ce
igor egorov <igor127@bk.ru>
2022-03-23 20:45:16 +0300

    add new cart with quantity and image, new button forms

--- PaintEquipment/Startup.cs
+++ PaintEquipment/Startup.cs
@@ -66,7 +66,7 @@ namespace PaintEquipment
                
 
             });
-            SeedData.EnsurePopulated(app);
+            //SeedData.EnsurePopulated(app);
             IdentitySeedData.EnsurePopulated(app);
         }
     }
